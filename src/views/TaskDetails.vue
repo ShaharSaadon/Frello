26d046613d5f0b4f9b26e2f770127ebcb0b80b09
Tomer Huberman <tomerh1999@gmail.com>
2023-03-22 15:30:21 +0200

    <TaskDetails> side bar and description

--- src/views/TaskDetails.vue
+++ src/views/TaskDetails.vue
@@ -4,22 +4,34 @@
       <div class="header">
         <span></span>
         <div class="title">
-          <textarea data-autosize="true">asdasdasdasdasdasdasdasd</textarea>
+          <!-- <h2>{{ rowCount }}</h2> -->
+          <textarea ref="textarea" v-model="taskToEdit.title" @keydown.enter.prevent="enter" hidden></textarea>
         </div>
         <p>in list {{ task.title }} </p>
+        <!-- <pre>{{ task }} </pre> -->
       </div>
-      <div class="main-content"></div>
+      <div class="main-content">
+        <!-- <TaskDescription :taskDescription="task.description" /> -->
+        <!-- <TaskChecklist :taskDescription="task.description" /> -->
+      </div>
       <div class="sidebar flex">
-        <h3>Suggested</h3>
-        <button class="button-link">Join</button>
+        <div class="flex space-between">
+          <h3>Suggested</h3>
+          <button class="gear"></button>
+        </div>
+        <button class="button-link member"> Join</button>
         <h3>Add to card</h3>
-        <button class="button-link">Members</button>
-        <button class="button-link">Labels</button>
-        <button class="button-link">Checklist</button>
-        <button class="button-link">Dates</button>
-        <button class="button-link">Attachment</button>
-        <button class="button-link">Cover</button>
-        <button class="button-link">Custom Fields</button>
+        
+        <button class="button-link member"> Members</button>
+        
+        <button class="button-link label"> Labels</button>
+        <button class="button-link checklist"> Checklist</button>
+        <button class="button-link clock"> Dates</button>
+        <button class="button-link attachment"> Attachment</button>
+        <button class="button-link card-cover"> Cover</button>
+        <!-- <button class="button-link"> Custom Fields</button> -->
+        <h3>Actions</h3>
+        <button class="button-link archive"> Archive</button>
       </div>
 
     </main>
@@ -30,35 +42,36 @@
 // import {boardService} from '../services/board.service'
 // import GroupList from '../cmps/GroupList.vue'
 import { svgService } from "../services/svg.service.js"
+import  TaskDescription  from "../cmps/TaskDescription.vue"
 
 export default {
   props: {
     task: {
       type: Object,
       required: true,
-    }
+    },
   },
   data() {
     return {
-      // board: null
+      taskToEdit: {}
     }
   },
   async created() {
-    // const board = await boardService.getById(id)
-    // this.board = board
+    this.taskToEdit = { ...this.task }
   },
   watch: {
   },
   computed: {
-
   },
   methods: {
-    // getSvg(iconName) {
-    //   return svgService.getTrelloSvg(iconName)
-    // },
+    enter(ev) {
+      console.log("ev: ", ev);
+      ev.preventDefault()
+      ev.target.blur()
+    },
   },
   components: {
-
+    TaskDescription
   }
 }
 </script>
