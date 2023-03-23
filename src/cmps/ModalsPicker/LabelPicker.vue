3d4d40c8a5771f7a9d1a8d4b1efb05595921d333
Tomer Huberman <tomerh1999@gmail.com>
2023-03-23 20:22:21 +0200

    <labelPicker> start

--- src/cmps/ModalsPicker/LabelPicker.vue
+++ src/cmps/ModalsPicker/LabelPicker.vue
@@ -1,41 +1,60 @@
 <template>
-<div class="label-picker">
+  <div class="label-picker">
     <h1>{{ info.label }}</h1>
-    <hr>
-    <input type="text" placeholder="Search Labels...">
+    <hr />
+    <input type="text" placeholder="Search Labels..." />
     <h3>Labels</h3>
-    
-    <button> Create a new label</button>
-</div>
-
+    <div class="label-picker-ops" v-for="label in labels" :key="label.color">
+      <span @click="toggleCheck(label)" class="check-box" :class="label.isChecked" v-html="getSvg('checkBox')"></span>
+      <div class="btn-label-tag label-tag"></div>
+      <button><span>e</span></button>
+    </div>
+    <button>Create a new label</button>
+  </div>
 </template>
 
 <script>
+import { svgService } from '../../services/svg.service.js'
 export default {
-    props: {
-        info: {
-            type: Object,
-            required: true,
-        }
+  props: {
+    info: {
+      type: Object,
+      required: true,
     },
-    name: '',
-    data() {
-        return {
-
-        }
+  },
+  name: '',
+  data() {
+    return {
+      labels: {
+        green: {
+          color: '',
+          title: '',
+          isChecked: false,
+        },
+        yellow: {
+          color: '',
+          title: '',
+          isChecked: false,
+        },
+        orange: {
+          color: '',
+          title: '',
+          isChecked: false,
+        },
+      },
+    }
+  },
+  methods: {
+    getSvg(iconName) {
+      return svgService.getTrelloSvg(iconName)
     },
-    methods: {
-
+    toggleCheck(isChecked) {
+  
     },
-    computed: {
-
-    },
-    created() {
-
-    },
-    components: {
-
-    },
+  },
+  computed: {},
+  created() {},
+  components: {},
 }
 </script>
 
