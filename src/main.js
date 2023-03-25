2e922234546d8147c13766eaef061427319f4366
Shahar Saadon <shaharsaadon1@gmail.com>
2023-03-25 22:11:01 +0300

    sync

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 
 import { router } from './router.js'
 import { store } from './store/store.js'
-import TextareaAutosize from 'vue-textarea-autosize'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -14,7 +13,6 @@ import App from './App.vue'
 const app = createApp(App)
 app.use(router)
 app.use(store)
-app.use(TextareaAutosize)
 app.use(ElementPlus)
 
 app.mount('#app')
