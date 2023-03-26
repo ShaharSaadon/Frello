6cc86732f08b6facf1e57699ff1746fd1ec35a84
Shahar Saadon <shaharsaadon1@gmail.com>
2023-03-26 22:27:57 +0300

    right side bar

--- src/cmps/BoardDetails/RightSideBar.vue
+++ src/cmps/BoardDetails/RightSideBar.vue
@@ -1,30 +1,39 @@
 <template>
-<div class="aside">cxa</div>
+    <aside class="right-side-bar" :class="counterClass">
+        <div class="right-side-toggle">
+            <div class="three-dot-btn" @click="isExpended = true" v-if="!isExpended"></div>
+            <div class="right-side" :hidden="!isExpended">
+                <h4>Menu</h4> <span @click="isExpended = false">X</span>
+            </div>
+        </div>
+    </aside>
 </template>
 
 <script>
 export default {
-name: '',
-data() {
-return {
+    name: '',
+    data() {
+        return {
+            isExpended: false
+        }
+    },
+    methods: {
 
-}
-},
-methods: {
+    },
+    computed: {
+        counterClass() {
+            return {
+                isExpended: this.isExpended,
+            }
+        }
+    },
+    created() {
 
-},
-computed: {
+    },
+    components: {
 
-},
-created() {
-
-},
-components: {
-
-},
+    },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
