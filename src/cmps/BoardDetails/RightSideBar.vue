f3fb7ec99d0f851c13dcd1ce036e94d569608bdf
Shahar Saadon <shaharsaadon1@gmail.com>
2023-03-26 18:53:45 +0300

    fixing css

---
+++ src/cmps/BoardDetails/RightSideBar.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="aside">cxa</div>
+</template>
+
+<script>
+export default {
+name: '',
+data() {
+return {
+
+}
+},
+methods: {
+
+},
+computed: {
+
+},
+created() {
+
+},
+components: {
+
+},
+}
+</script>
+
+<style>
+
+</style>
