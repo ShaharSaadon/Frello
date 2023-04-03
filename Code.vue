2cc851c795d1a49abad19ff73cbbbffb86b5087d
Shahar Saadon <shaharsaadon1@gmail.com>
2023-04-03 09:11:26 +0300

    sync

--- Code.vue
+++ Code.vue
@@ -1,31 +1,30 @@
-<script>
-export default {
-  methods: {
-
-    // TaskDetails Component
-    updateEntityVal({ key, val }) {
-      let activity
-      const valToChange = JSON.parse(JSON.stringify(this.task[key]))
-      const itemId = val.id ?? val
-      // finds the item index and pushes or removes
-      const idx = valToChange.findIndex((item) => item.id === itemId)
-      if (idx === -1) {
-        valToChange.push(val)
-        activity = ['added', `${key.slice(0, -1)}`, 'to', this.task.title]
-      } else {
-        valToChange.splice(idx, 1, val)
-        activity = ['updated', `${key.slice(0, -1)}`, 'from', this.task.title]
-
-      }
-      this.saveTask({ key, newVal: valToChange, activity })
+  <script>
+  export default {
+    methods: {
+      // TaskDetails Component
+      updateEntityVal({ key, val }) {
+        let activity
+        const entityValues = JSON.parse(JSON.stringify(this.task[key]))
+        
+        // Differentiate between two types of arrays
+        // an array of objects and an array of IDs
+        const itemId = val.id ? val.id : val
+        const idx = entityValues.findIndex((item) => item.id === itemId)
+        if (idx === -1) {
+          entityValues.push(val)
+          activity = ['added', `${key.slice(0, -1)}`, 'to', this.task.title]
+        } else {
+          entityValues.splice(idx, 1, val)
+          activity = ['updated', `${key.slice(0, -1)}`, 'from', this.task.title]
+        }
+        this.saveTask({ key, newVal: entityValues, activity })
+      },
+      // Task Activities Component
+      addComment() {
+        this.$emit('updateEntityVal', { key: 'comments', val: this.newComment })
+        this.newComment = boardService.getEmptyComment()
+        this.isOnEdit = false
+      },
     },
-
-    // Task Activities Component
-    addComment() {
-      this.$emit('updateEntityVal', { key: 'comments', val: this.newComment })
-      this.newComment = boardService.getEmptyComment()
-      this.isOnEdit = false
-    },
-  },
-}
-</script>
+  }
+  </script>
--- src/cmps/RightSideBar/SideBarBoardDetails.vue
+++ src/cmps/RightSideBar/SideBarBoardDetails.vue
@@ -1,21 +1,34 @@
 <template>
     <h4>Board admins</h4>
     <BoardMembers />
+    <button @click="generateCompletion">Generate Completion</button>
 
     <h4>Description</h4>
 </template>
 
 <script>
 import BoardMembers from '../BoardDetails/BoardMembers.vue';
+import { OpenAIApi } from 'openai';
+import axios from 'axios';
+
 export default {
     name: 'SideBarBoardDetails',
     data() {
         return {
-
         }
     },
     methods: {
-
+        async generateCompletion() {
+            const response = await this.openai.createCompletion({
+                model: 'text-davinci-002',
+                prompt: 'Hello world',
+                max_tokens: 50,
+                n: 1,
+                stop: ['\n'],
+                temperature: 0.5,
+            });
+            console.log(response.data.choices[0].text);
+        }
     },
     computed: {
         board() {
@@ -23,7 +36,15 @@ export default {
         },
     },
     created() {
-
+        const openaiAxios = axios.create({
+            baseURL: 'https://api.openai.com/v1/',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ssk-V4USteB9w1PlsH7pGnzjT3BlbkFJoNjoybdmty8VygBuS4XW`,
+            },
+        });
+        this.openai = new OpenAIApi(openaiAxios);
+        this.generateCompletion()
     },
     components: {
         BoardMembers
